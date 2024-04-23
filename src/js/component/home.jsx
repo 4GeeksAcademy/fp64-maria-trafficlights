b96ac3ea2e715d0cd140c85957fca73034ad6e1f
MaríaSF <158471273+DexpinBe@users.noreply.github.com>
2024-04-23 16:57:22 +0000

    Semáforo acabado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,96 @@
-import React from "react";
+import React from 'react';
+import { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const Semaforo = () => {
+  const [color, setColor] = useState('red');
+  const [intervalId, setIntervalId] = useState(null);
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+  const setPurple = () => {
+	setColor('purple');
+	const id = setTimeout(() =>{
+		setGreen();
+	}, 1000); 
+  }  
+  const setGreen = () => {
+    setColor('green');
+	const id = setTimeout(() => {
+		setYellow();
+	  }, 1000);
+	  setIntervalId(id);
+	};
 
-export default Home;
+  const setYellow = () => {
+    setColor('yellow');
+    const id = setTimeout(() => {
+      setRed();
+    }, 1000); 
+	setIntervalId(id)
+  };
+ 
+  const setRed = () => {
+    setColor('red');
+    const id = setTimeout(() => {
+      setPurple();
+    }, 1000); 
+	setIntervalId(id)
+  };
+
+  useEffect(() => {
+    setPurple();
+  }, []);
+
+  const stopInterval = () => {
+    clearInterval(intervalId);
+  };
+
+  return (
+    <div className="text-center">
+      <div
+        style={{
+          backgroundColor: color === 'red' ? 'red' : 'black',
+          width: '100px',
+          height: '100px',
+          borderRadius: '50%',
+          margin: '10px auto',
+          boxShadow: '0 0 10px rgba(0, 0, 0, 0.5)',
+        }}
+      ></div>
+      <div
+        style={{
+          backgroundColor: color === 'yellow' ? 'yellow' : 'black',
+          width: '100px',
+          height: '100px',
+          borderRadius: '50%',
+          margin: '10px auto',
+          boxShadow: '0 0 10px rgba(0, 0, 0, 0.5)',
+        }}
+      ></div>
+      <div
+        style={{
+          backgroundColor: color === 'green' ? 'green' : 'black',
+          width: '100px',
+          height: '100px',
+          borderRadius: '50%',
+          margin: '10px auto',
+          boxShadow: '0 0 10px rgba(0, 0, 0, 0.5)',
+        }}
+      ></div>
+	  <div
+        style={{
+          backgroundColor: color === 'purple' ? 'purple' : 'black',
+          width: '100px',
+          height: '100px',
+          borderRadius: '50%',
+          margin: '10px auto',
+          boxShadow: '0 0 10px rgba(0, 0, 0, 0.5)',
+        }}
+      ></div>
+
+	  <button className="btn btn-danger" onClick={stopInterval}>Parar el semáforo</button>
+    </div>
+  );
+}
+
+
+export default Semaforo;
